e5c7c696ef3e4340d1b465b5fc2c517a429a4e7f
Dinesh <dinesh@annantasourcellc.onmicrosoft.com>
2024-07-11 14:36:33 +0530

    List Rendering
    For Loop Function
    v-for="variant in variants" - {{ variant }}
    
    Setting the key attribute
    :key="variant.id"
    Gives each DOM element a unique key

--- main.js
+++ main.js
@@ -3,8 +3,13 @@ const app = Vue.createApp({
         return {
             product: 'Socks',
             image: './assets/images/socks_blue.jpg',
-            inStock: true,
-            details: ['50% cotton', '30% wool', '20% polyester']
+            inStock: false,
+            details: ['50% cotton', '30% wool', '20% polyester', '05% silk'],
+            sizes: ['S', 'M', 'L', 'XL', 'XXL'],
+            variants: [
+                {id: 1101, color: 'green'},
+                {id: 1102, color: 'blue' },
+            ]
         }
     }
 })
